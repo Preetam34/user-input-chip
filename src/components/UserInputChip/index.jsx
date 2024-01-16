5a3e1a8435050f1974d68b1fa76f9c2159a5d623
codepreetam34 <preetamkumar3232@mail.com>
2024-01-16 15:57:50 +0530

    added optimized changes

--- src/components/UserInputChip/index.jsx
+++ src/components/UserInputChip/index.jsx
@@ -6,7 +6,7 @@ const UserInputChip = () => {
   const [users, setUsers] = useState(userData);
   const [selectedUsers, setSelectedUsers] = useState([]);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  
+
   const handleInputChange = (event) => {
     let value = event.target.value;
     setInputValue(value);
@@ -23,9 +23,6 @@ const UserInputChip = () => {
 
   const handleInputClick = () => {
     setIsDropdownOpen(!isDropdownOpen);
-    if (!isDropdownOpen) {
-      setUsers(userData);
-    }
   };
 
   const handleUserSelect = (user) => {
@@ -40,65 +37,70 @@ const UserInputChip = () => {
   };
 
   return (
-
-  
     <div className="user-dropdown-container">
       <h2 className="dropdown-title">Pick Users</h2>
-      <div className="selected-chips-container">
-        {selectedUsers.map((user) => (
-          <div
-            key={"user-" + user.id}
-            className="chip"
-            onClick={() => handleChipRemove(user)}
-          >
-            <img
-              src={user.profilePic}
-              alt={user.name}
-              className="profile-icon-chip"
-            />
-            {user.name}
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="cross-icon"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </span>
-          </div>
-        ))}
-      </div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        onClick={handleInputClick}
-        placeholder="Add a new user...."
-        className="input-field"
-      />
-      {isDropdownOpen && (
-        <ul className="dropdown-list">
-          {users.map((user) => (
-            <li key={"user-" + user.id} onClick={() => handleUserSelect(user)}>
+      <div className="adjust-size">
+        <div className="selected-chips-container">
+          {selectedUsers.map((user) => (
+            <div
+              key={"user-" + user.id}
+              className="chip"
+              onClick={() => handleChipRemove(user)}
+            >
               <img
-                className="profile-icon"
                 src={user.profilePic}
                 alt={user.name}
+                className="profile-icon-chip"
               />
-              <span className="user-name">{user.name} </span>
-              <span className="user-email"> {user.email}</span>
-            </li>
+              {user.name}
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="cross-icon"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </span>
+            </div>
           ))}
-        </ul>
-      )}
+        </div>
+        <div style={{ position: "relative" }}>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            onClick={handleInputClick}
+            placeholder="Add a new user...."
+            className="input-field"
+          />
+          {isDropdownOpen && (
+            <ul className="dropdown-list">
+              {users.map((user) => (
+                <li
+                  key={"user-" + user.id}
+                  onClick={() => handleUserSelect(user)}
+                >
+                  <img
+                    className="profile-icon"
+                    src={user.profilePic}
+                    alt={user.name}
+                  />
+                  <span className="user-name">{user.name} </span>
+                  <span className="user-email"> {user.email}</span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
